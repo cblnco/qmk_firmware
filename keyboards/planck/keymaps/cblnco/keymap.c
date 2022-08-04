b6dbbb0eed74daabb70afcbae0079f368ce69ad3
cblnco <jcarlos.kb@gmail.com>
2022-08-03 23:13:47 -0500

    Modified keeb layout

--- keyboards/planck/keymaps/cblnco/keymap.c
+++ keyboards/planck/keymaps/cblnco/keymap.c
@@ -36,7 +36,7 @@ enum planck_keycodes {
 
 #define MOBILITY MO(_MOBILITY)
 #define NUMBERS MO(_NUMBERS)
-#define SYMBOLS MO(_SYMBOLS)
+#define SYMBOLS TG(_SYMBOLS)
 #define FUNCTION MO(_FUNCTION)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,     KC_P,     KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,   KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
     LSFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_ENT ,
-    SYMBOLS, KC_LALT, KC_LGUI, KC_LCTL, NUMBERS, KC_SPC, KC_SPC, MOBILITY, KC_RCTRL, KC_RALT, KC_RGUI,  KC_RSFT
+    LSFT_T(SYMBOLS), KC_LALT, KC_LGUI, KC_LCTL, NUMBERS, KC_SPC, KC_SPC, MOBILITY, KC_RCTRL, KC_RALT, KC_RGUI,  RSFT_T(SYMBOLS)
 ),
 
 /* Mobility
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_VOLD, KC_UP,   KC_VOLU,  KC_R,    KC_T,   KC_Y,   KC_U,     KC_HOME,  KC_UP,   KC_END,   KC_BSPC,
     KC_TAB,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_F,    KC_G,   KC_H,   KC_J,     KC_LEFT,  KC_DOWN, KC_RIGHT, KC_QUOT,
     LSFT_T(KC_CAPS), KC_MUTE, KC_X,    KC_C,     KC_V,    KC_B,   KC_N,   KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_ENT ,
-    SYMBOLS, KC_LALT, KC_LGUI, KC_LCTL,  NUMBERS, KC_SPC, KC_SPC, KC_TRNS, KC_RCTRL, KC_RALT, KC_RGUI,  KC_RSFT
+    LSFT_T(SYMBOLS), KC_LALT, KC_LGUI, KC_LCTL,  NUMBERS, KC_SPC, KC_SPC, KC_TRNS, KC_RCTRL, KC_RALT, KC_RGUI,  RSFT_T(SYMBOLS)
 ),
 
 /* Numbers / Functions
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_T,   KC_Y,   KC_U,     KC_1,     KC_2,    KC_3,    KC_BSPC,
     KC_TAB,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_G,   KC_H,   KC_J,     KC_4,     KC_5,    KC_6,    KC_QUOT,
     LSFT_T(KC_CAPS), KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_B,   KC_N,   KC_0,     KC_7,     KC_8,    KC_9,    KC_ENT ,
-    SYMBOLS, KC_LALT, KC_LGUI, KC_LCTL, KC_TRNS, KC_SPC, KC_SPC, MOBILITY, KC_RCTRL, KC_RALT, KC_RGUI, KC_RSFT
+    LSFT_T(SYMBOLS), KC_LALT, KC_LGUI, KC_LCTL, KC_TRNS, KC_SPC, KC_SPC, MOBILITY, KC_RCTRL, KC_RALT, KC_RGUI, RSFT_T(SYMBOLS)
 ),
 
 /* Symbols
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC,   KC_LPRN, KC_RPRN,    KC_MINS,     KC_EQL,     KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_AMPR, KC_ASTR, KC_H,   KC_LCBR, KC_RCBR,    KC_LBRC,  KC_RBRC,  KC_BSLASH,
     LSFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_ENT ,
-    KC_TRNS, KC_LALT, KC_LGUI, KC_LCTL, NUMBERS, KC_SPC,  KC_SPC, MOBILITY, KC_RCTRL, KC_RALT, KC_RGUI,  KC_RSFT
+    KC_TRNS, KC_LALT, KC_LGUI, KC_LCTL, NUMBERS, KC_SPC,  KC_SPC, MOBILITY, KC_RCTRL, KC_RALT, KC_RGUI,  RSFT_T(SYMBOLS)
 ),
 
 /* Adjust (Lower + Raise)
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______,   _______,   _______, _______, _______, _______, _______, _______,  _______, _______, _______ ,
     _______, _______, _______,  _______,   _______,  _______, _______, _______,  _______,  _______,  _______,  _______,
     _______, _______,  _______,  _______,   _______,  _______,   _______,  _______, _______, _______, _______, _______,
-    LCG_NRM, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, LCG_SWP
+    MAGIC_UNSWAP_LCTL_LGUI, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, MAGIC_SWAP_LCTL_LGUI
 )
 
 };
@@ -163,7 +163,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case AG_NORM:
+    case MAGIC_UNSWAP_LCTL_LGUI:
       if (record->event.pressed) {
         #ifdef AUDIO_ENABLE
           PLAY_SONG(lcg_norm);
@@ -171,7 +171,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return true;
       break;
-    case AG_SWAP:
+    case MAGIC_SWAP_LCTL_LGUI:
       if (record->event.pressed) {
         #ifdef AUDIO_ENABLE
           PLAY_SONG(lcg_swap);
